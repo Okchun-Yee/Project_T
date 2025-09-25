cd5866a8d83e60abcf6af242cca926e53f1498f2
CHG1521 <gusrb1521@naver.com>
2025-09-25 17:36:21 +0900

    작엽 환경 이동

--- Assets/Scripts/Management/UIManger.cs
+++ Assets/Scripts/Management/UIManger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,28 +9,28 @@ public class UIManger : Singleton<UIManger>
     private Slider healthSlider;
     private int currentHealth;
     [SerializeField] private int maxHealth = 100;
+    public TextMeshProUGUI hpText; //hp 표시 텍스트
 
     protected override void Awake()
     {
         base.Awake();
         currentHealth = maxHealth;
     }
-    private void Start()
+
+     private void Start()
     {   
         Debug.Log("테스트 시작");
         currentHealth = maxHealth;
         UpdateHealthSlider();
         
-        // 1초마다 TakeDamage() 실행
+        // 2초마다 TakeDamage() 실행
         InvokeRepeating("TakeDamage", 2, 2);
     }
-
 
     private void TakeDamage()
     {
         currentHealth -= 10;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // 0 이하로는 안 떨어지게
-        Debug.Log("Hp감소");
         UpdateHealthSlider();
     }
     private void UpdateHealthSlider()
@@ -41,5 +42,6 @@ public class UIManger : Singleton<UIManger>
         }
         healthSlider.maxValue = maxHealth;
         healthSlider.value = currentHealth;
+        hpText.text = currentHealth + "/" + maxHealth;
     }
 }
