df042651b59aa78d9a889083a63c41f5df1cf2d6
CHG1521 <gusrb1521@naver.com>
2025-10-21 10:39:43 +0900

    work: 작업 환경 이동

--- Assets/Scripts/Inventory/MouseFollower.cs
+++ Assets/Scripts/Inventory/MouseFollower.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class MouseFollower : MonoBehaviour
 {
     [SerializeField] private Canvas canvas;
-    [SerializeField] private Camera mainCam;
     [SerializeField] private InventoryItem item;
 
     public void Awake()
     {
         canvas = transform.root.GetComponent<Canvas>();
-        mainCam = Camera.main;
         item = GetComponentInChildren<InventoryItem>();
     }
 
     public void SetData(Sprite sprite, int quantity)
     {
         item.SetData(sprite, quantity);
+    }
+    private void Update()
+    {
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)canvas.transform,
+            Input.mousePosition,
+            canvas.worldCamera,
+            out position);
+        transform.position = canvas.transform.TransformDirection(position);
+    }
+    public void Toggle(bool val)
+    {
+        Debug.Log($"Item Toggled {val}");
+        gameObject.SetActive(val);
     }
 }
