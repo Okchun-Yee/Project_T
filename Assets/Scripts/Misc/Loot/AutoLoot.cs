f67e37e75fc7a7cf3310f9e557b7e4e211041e0c
한채훈 <han020813@gmail.com>
2025-11-25 13:33:19 +0900

    refactor: readonly 제거

--- Assets/Scripts/Misc/Loot/AutoLoot.cs
+++ Assets/Scripts/Misc/Loot/AutoLoot.cs
@@ -13,12 +13,12 @@ public class AutoLoot : MonoBehaviour
 
     [SerializeField] readonly private AutoLootType lootType;    // 자동 루팅 아이템 타입
     [SerializeField] private float lootDistance = 5f;           // 플레이어가 아이템을 감지하는 거리
-    [SerializeField] private readonly float accelerationRate = .2f; // 플레이어에게 다가갈 때 가속도
+    [SerializeField] private float accelerationRate = .2f; // 플레이어에게 다가갈 때 가속도
     [SerializeField] private float moveSpeed = 3f;              // 아이템 이동 속도
     [SerializeField] private float maxMoveSpeed = 30f;          // 아이템 최대 이동 속도
     [SerializeField] private AnimationCurve animCurve;          // 아이템이 튀어오르는 애니메이션 커브
-    [SerializeField] private readonly float heightY = 1.5f;     // 아이템이 튀어오르는 높이
-    [SerializeField] private readonly float popDuration = 1f;   // 아이템이 튀어오르는 애니메이션 지속 시간 
+    [SerializeField] private float heightY = 1.5f;     // 아이템이 튀어오르는 높이
+    [SerializeField] private float popDuration = 1f;   // 아이템이 튀어오르는 애니메이션 지속 시간 
     
     private Vector3 moveDir;
     private Rigidbody2D rb;
