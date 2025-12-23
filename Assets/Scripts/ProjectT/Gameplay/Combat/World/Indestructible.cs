25c6de92f3680ec3787db1dfa0a00b5f482b471a
한채훈 <han020813@gmail.com>
2025-12-23 20:45:25 +0900

    style: Indestructible.cs 주석 추가

--- Assets/Scripts/ProjectT/Gameplay/Combat/World/Indestructible.cs
+++ Assets/Scripts/ProjectT/Gameplay/Combat/World/Indestructible.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace ProjectT.Gameplay.Combat.World
 {
+    /// <summary>
+    /// 파괴 불가능 오브젝트 표시 컴포넌트 (Marker)
+    /// </summary>
     public class Indestructible : MonoBehaviour
     {
-    
+        public bool IsIndestructible => true;
     }
 }
